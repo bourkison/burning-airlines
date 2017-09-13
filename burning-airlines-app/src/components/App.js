be49bae5d31f009520643614f6c9a908f4b5ba0b
bourkison <harrisonbourke97@gmail.com>
2017-09-13 10:29:49 +1000

    reservation.js

--- burning-airlines-app/src/components/App.js
+++ burning-airlines-app/src/components/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Reservation from './Reservation';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <FlightsShow />
         <Reservation />
       </div>
     );
---
+++ burning-airlines-app/src/components/Reservation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Reservation extends Component {
+
+  render() {
+    return (
+      <h1>Hey</h1>
+    );
+  }
+
+}
+
+export default Reservation
