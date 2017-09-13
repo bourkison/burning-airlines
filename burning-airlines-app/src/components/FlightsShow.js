b72f270c07e3f23157547de495f879914743241a
Mina Parnian <minaparnian66@gmail.com>
2017-09-13 15:39:16 +1000

    css

--- burning-airlines-app/src/components/FlightsShow.js
+++ burning-airlines-app/src/components/FlightsShow.js
@@ -4,47 +4,59 @@ import axios from 'axios';
 
 const SERVER_URL = 'http://localhost:5000/flights.json';
 
+function AllFlights(props) {
+  let rows = [];
+  for (let row = 0; row < props.flights.length; row++) {
+    let cols = [];
+    for (let col = 0; col <= 4; col++) {
+      if (col === 0) {
+        cols.push(<td>{props.flights[row].date}</td>);
+      } else if (col === 1) {
+        cols.push(<td>{props.flights[row].flight_number}</td>);
+      } else if (col === 2) {
+        cols.push(<td>{props.flights[row].from} > {props.flights[row].to}</td>);
+      }
+      else {
+        cols.push(<td>{props.flights[row].airplane_id}</td>);
+      }
+    }
+    rows.push(<tr key={rows}>{cols}</tr>)
+  }
+
+  return (
+    <table>
+      <tr>
+        <td>Date</td>
+        <td>Flight</td>
+        <td>From > To</td>
+        <td>Plane</td>
+      </tr>
+      {rows}
+    </table>
+  )
+
+
+}
+
 class Flights extends Component {
   constructor(props){
     super(props);
     this.state = {flights: []};
-    this.saveFlight = this.saveFlight.bind(this);
 
     const fetchFlights = () => {
       axios.get(SERVER_URL).then(function(results) {
-        console.log(results);
         this.setState({flights: results.data});
+        console.log(this.state.flights);
       }.bind(this));
     }
     fetchFlights();
   }
-  saveFlight(content) {
-    //save the flight to the server using axios
-    axios.post(SERVER_URL, {content: content}).then(function(result){
-      console.log(result);
-    });
-  }
 
-  flightInfo(){
-    <div>
-        { this.props.flights.map( (f) => <p key={f.id}>{f.content}</p> ) }
-    </div>
-  }
+
 
   render() {
     return (
-      <div>
-        <h2>Flight Search Results</h2>
-
-        <table>
-        <tr>
-          <td>Date</td>
-          <td>Flight</td>
-          <td>From > To</td>
-          <td>Plane</td>
-        </tr>
-      </table>
-      </div>
+      <AllFlights flights={this.state.flights} />
     )
   }
 }
